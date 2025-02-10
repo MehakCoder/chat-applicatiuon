f7e125e4aaf0e46091fdcdeba024f59968769231
MehakCoder <cmehak26@gmail.com>
2025-02-10 20:58:09 +0530

    add new folder and file data

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { chats } = require("./data/data");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is Running ");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  console.log(req.params.id);
+});
+
+app.listen(5000, console.log("Server Started on PORT 5000"));
