2cf4a333cea72aa5920e0f930a8a60a14cb6dea0
MehakCoder <cmehak26@gmail.com>
2025-02-12 20:59:27 +0530

    add new server and new setting in it

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-  res.send("API is Running ");
+  res.send("API is Running  Successfully");
 });
 
 app.get("/api/chat", (req, res) => {
@@ -12,7 +14,11 @@ app.get("/api/chat", (req, res) => {
 });
 
 app.get("/api/chat/:id", (req, res) => {
-  console.log(req.params.id);
+  // console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
 });
 
-app.listen(5000, console.log("Server Started on PORT 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`));
